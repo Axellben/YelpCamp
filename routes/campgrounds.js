beae251d36af705354ef288f879184954cd1fb3b
Axellben <cretu.alexandru2000@gmail.com>
2021-07-27 16:33:20 +0300

    refact

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,17 +3,22 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router
   .route("/")
   // Route to show all campgrounds
   .get(catchAsync(campgrounds.index))
   // Route to handle the POST request to create a new campground
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.single("image"), (req, res) => {
+    res.send(req.body, req.file);
+  });
 
 // Route to get the form to create a new campground
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
